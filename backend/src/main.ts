49ca14eaeec37d0aee559bbd80756f5912eeaaa2
NedimKurtulus <nedimkrtls7@gmail.com>
2026-01-08 21:17:29 +0300

    Refactor global pipes and CORS configuration

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,17 +5,27 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(new ValidationPipe({
-    transform: true,  // BU SATIRI EKLE
-    transformOptions: {
-      enableImplicitConversion: true,
-    },
-  }));
-  app.enableCors({
-  origin: [
-    'http://localhost:3000',
-    'https://my-project-9ga6ox8sc-nks-projects-b2b2385a.vercel.app',
-  ],
-  credentials: true,
-});
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
 
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'https://my-project-9ga6ox8sc-nks-projects-b2b2385a.vercel.app',
+    ],
+    credentials: true,
+  });
+
+  const PORT = process.env.PORT || 3001;
+  await app.listen(PORT, '0.0.0.0');
+
+  console.log(`🚀 Server running on port ${PORT}`);
+}
+
+bootstrap();
