5f8b5e0be129a403ab4bcc240a9675509b3c21fa
NedimKurtulus <nedimkrtls7@gmail.com>
2026-01-08 21:12:33 +0300

    Update api.ts

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,17 +1,18 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3001';
+const API_URL = process.env.REACT_APP_API_URL;
 
 const api = axios.create({
-    baseURL: API_URL,
+  baseURL: API_URL,
+  withCredentials: true,
 });
 
 api.interceptors.request.use((config) => {
-    const token = localStorage.getItem('token');
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 });
 
 export default api;
